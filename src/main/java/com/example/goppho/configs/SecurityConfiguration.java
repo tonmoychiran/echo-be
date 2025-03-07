034f4cc54369a31dbf6f40a44361ab40c2272e8f
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-03-07 21:36:19 +0600

    fix: new SecurityConfiguration

--- src/main/java/com/example/goppho/configs/SecurityConfiguration.java
+++ src/main/java/com/example/goppho/configs/SecurityConfiguration.java
@@ -2,15 +2,17 @@ package com.example.goppho.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@EnableWebSecurity
 @Configuration
-public class ApplicationNoSecurity {
+public class SecurityConfiguration {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .requestMatchers(new AntPathRequestMatcher("/**"));
+        return (web) -> web.ignoring().requestMatchers("/api/v1/auth/*");
     }
+
+
 }
