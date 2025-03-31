00928586d2a17788786661460ce356d5a9e62e87
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-03-31 14:35:18 +0600

    feat: single file upload worked

---
+++ src/main/java/com/example/goppho/controllers/FileUploadController.java
@@ -0,0 +1,33 @@
+package com.example.goppho.controllers;
+
+import com.example.goppho.services.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/file")
+public class FileUploadController {
+
+    private FileStorageService fileStorageService;
+
+    @Autowired
+    public FileUploadController(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
+    }
+
+    @PostMapping("/upload-file")
+    public Boolean uploadFile(
+            @RequestParam("files") MultipartFile files
+    ) throws IOException {
+        System.out.println(files);
+        return true;
+    }
+}
