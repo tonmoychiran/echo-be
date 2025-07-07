ee85c28b162237df727b2cc5cf93abcf272f82b9
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-07 19:28:09 +0600

    refactor: moved /profile api from /auth to /user

--- src/main/java/com/example/goppho/controllers/UserAuthController.java
+++ src/main/java/com/example/goppho/controllers/UserAuthController.java
@@ -1,10 +1,8 @@
 package com.example.goppho.controllers;
 
-import com.example.goppho.entities.UserPrincipalEntity;
+
 import com.example.goppho.requests.UserLoginOTPRequest;
-import com.example.goppho.requests.UserLoginOTPResendRequest;
 import com.example.goppho.responses.Response;
-import com.example.goppho.responses.UserLoginOTPResponse;
 import com.example.goppho.requests.UserLoginVerificationRequest;
 import com.example.goppho.responses.VerifiedUserLoginResponse;
 import com.example.goppho.services.UserAuthService;
@@ -12,7 +10,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -33,7 +30,7 @@ public class UserAuthController {
         Response response = this.userAuthService.generateUserLoginOTP(
                 userLoginRequest
         );
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/verification")
@@ -44,18 +41,5 @@ public class UserAuthController {
                 userLoginVerifyRequest
         );
         return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    @GetMapping("/profile")
-    public ResponseEntity<UserPrincipalEntity> getUserProfile(@AuthenticationPrincipal UserPrincipalEntity userPrincipal) {
-
-/*        if (userPrincipal == null) {
-            return ResponseEntity.badRequest().body("User not authenticated.");
-        }
-
-        String userId = userPrincipal.getUsername(); // Get the user ID
-        //Or any other information from the userPrincipal.*/
-        return ResponseEntity.ok(userPrincipal);
-//        {"enabled":true,"accountNonLocked":true,"username":"46f1c84d-674c-4232-af34-ec80132cdf75","authorities":[],"credentialsNonExpired":true,"accountNonExpired":true,"password":""}
     }
 }
