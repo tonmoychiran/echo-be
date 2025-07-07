f95bdef28be69ca5ba2878244c7f8b26c0af6ecb
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-07 19:25:04 +0600

    feat: set default time zone to Dhaka

--- src/main/java/com/example/goppho/GopphoApplication.java
+++ src/main/java/com/example/goppho/GopphoApplication.java
@@ -3,10 +3,13 @@ package com.example.goppho;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class GopphoApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+06:00"));
         SpringApplication.run(GopphoApplication.class, args);
     }
 
