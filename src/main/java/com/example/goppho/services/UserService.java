3f502368f80af71faee5022f282eddd18b33d3e4
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-07 19:35:53 +0600

    feat: added update name, dob service feat: added get user information by email and name

--- src/main/java/com/example/goppho/services/UserService.java
+++ src/main/java/com/example/goppho/services/UserService.java
@@ -1,23 +1,36 @@
 package com.example.goppho.services;
 
+import com.example.goppho.entities.UserAuthOTPEntity;
 import com.example.goppho.entities.UserEntity;
+import com.example.goppho.entities.UserInformationEntity;
+import com.example.goppho.repositories.UserInformationRepository;
 import com.example.goppho.repositories.UserRepository;
+import com.example.goppho.requests.UserDOBUpdateRequest;
+import com.example.goppho.requests.UserNameUpdateRequest;
+import com.example.goppho.responses.Response;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
     UserRepository userRepository;
+    UserInformationRepository userInformationRepository;
 
     @Autowired
     public UserService(
-            UserRepository userRepository
+            UserRepository userRepository,
+            UserInformationRepository userInformationRepository
     ) {
         this.userRepository = userRepository;
+        this.userInformationRepository = userInformationRepository;
     }
 
     @Transactional
@@ -30,9 +43,59 @@ public class UserService {
         return this.userRepository.findById(userId);
     }
 
-    public UserEntity getUserByEmail(String email) {
-        Optional<UserEntity> user = this.userRepository.findByUserEmail(email);
-        return user.orElse(null);
+    public UserInformationEntity getUserInformationByEmail(UserDetails userDetails) {
+        UserAuthOTPEntity userAuthOTPEntity = (UserAuthOTPEntity) userDetails;
+        UserEntity user = userAuthOTPEntity.getUser();
+
+        Optional<UserInformationEntity> userInformation = this.userInformationRepository.findByUser(user);
+        return userInformation.orElseThrow(() -> new EntityNotFoundException("User not found"));
     }
 
+    public UserInformationEntity getUserByName(String name) {
+        Optional<UserInformationEntity> user = this.userInformationRepository.findByName(name);
+        return user.orElseThrow(() -> new EntityNotFoundException("User not found"));
+    }
+
+    public Response updateUserName(
+            UserDetails userDetails,
+            UserNameUpdateRequest userNameUpdateRequest
+    ) {
+        String name = userNameUpdateRequest.getName();
+
+        UserAuthOTPEntity userAuthOTPEntity = (UserAuthOTPEntity) userDetails;
+        UserEntity user = userAuthOTPEntity.getUser();
+        Optional<UserInformationEntity> userInformation = this.userInformationRepository.findByUser(user);
+
+        if (userInformation.isPresent()) {
+            UserInformationEntity updatableUserInformationEntity = userInformation.get();
+            updatableUserInformationEntity.setName(name);
+            this.userInformationRepository.save(updatableUserInformationEntity);
+        } else {
+            this.userInformationRepository.save(new UserInformationEntity(name, user));
+        }
+
+        return new Response("Updated");
+    }
+
+    public Response updateUserDOB(
+            UserDetails userDetails,
+            UserDOBUpdateRequest userDOBUpdateRequest
+    ) {
+        LocalDate localDate = userDOBUpdateRequest.getDob();
+        Date dob = Date.valueOf(localDate);
+
+        UserAuthOTPEntity userAuthOTPEntity = (UserAuthOTPEntity) userDetails;
+        UserEntity user = userAuthOTPEntity.getUser();
+        Optional<UserInformationEntity> userInformation = this.userInformationRepository.findByUser(user);
+
+        if (userInformation.isEmpty()) {
+            throw new EntityNotFoundException("Update failed");
+        }
+
+        UserInformationEntity updatableUserInformationEntity = userInformation.get();
+        updatableUserInformationEntity.setDob(dob);
+        this.userInformationRepository.save(updatableUserInformationEntity);
+
+        return new Response("Updated");
+    }
 }
