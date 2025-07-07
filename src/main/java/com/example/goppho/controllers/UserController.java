c9336ba0f77cfe2c071f8261de2cf6c797676ac4
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-07 19:29:19 +0600

    feat: added update name, dob api feat: added get profile api

--- src/main/java/com/example/goppho/controllers/UserController.java
+++ src/main/java/com/example/goppho/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.example.goppho.controllers;
 
 import com.example.goppho.entities.UserEntity;
+import com.example.goppho.entities.UserInformationEntity;
+import com.example.goppho.requests.UserDOBUpdateRequest;
+import com.example.goppho.requests.UserNameUpdateRequest;
+import com.example.goppho.responses.Response;
 import com.example.goppho.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -20,12 +24,39 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping()
+    @GetMapping("/search")
     public ResponseEntity<UserEntity> searchByEmail(
             @RequestParam("email") String email
     ) {
         UserEntity user = this.userService.getUserByEmail(email);
         return ResponseEntity.ok(user);
     }
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserInformationEntity> getUserProfile(
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        UserInformationEntity user = this.userService.getUserInformationByEmail(userDetails);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/name")
+    public ResponseEntity<Response> updateUserUserName(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @Valid @RequestBody UserNameUpdateRequest userNameUpdateRequest
+    ) {
+        Response response = this.userService.updateUserName(userDetails, userNameUpdateRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/dob")
+    public ResponseEntity<Response> updateUserUserDOB(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @Valid @RequestBody UserDOBUpdateRequest userDOBUpdateRequest
+    ) {
+        Response response = this.userService.updateUserDOB(userDetails, userDOBUpdateRequest);
+        return ResponseEntity.ok(response);
+    }
+
 
 }
