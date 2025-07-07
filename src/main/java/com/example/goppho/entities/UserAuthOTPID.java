eb76a8a29d27b9ae82557bacf342fd492faa4ae6
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-07 19:28:28 +0600

    refactor:

--- src/main/java/com/example/goppho/entities/UserAuthOTPID.java
+++ src/main/java/com/example/goppho/entities/UserAuthOTPID.java
@@ -1,9 +1,5 @@
 package com.example.goppho.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
-import org.apache.catalina.User;
-
 import java.io.Serializable;
 import java.util.Objects;
 
