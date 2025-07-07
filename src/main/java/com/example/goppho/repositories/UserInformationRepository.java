766f871682f743629784929fbf9b3ce79b751b02
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-07 19:36:10 +0600

    feat: added get user information by name query

--- src/main/java/com/example/goppho/repositories/UserInformationRepository.java
+++ src/main/java/com/example/goppho/repositories/UserInformationRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 
 public interface UserInformationRepository extends JpaRepository<UserInformationEntity, String> {
     Optional<UserInformationEntity> findByUser(UserEntity user);
+    Optional<UserInformationEntity> findByName(String name);
 }
