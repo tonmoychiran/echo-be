c02fc16dae61ddd5561a63cc8c266c3f8bee8d09
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-07 19:30:44 +0600

    feat: added user information table

---
+++ src/main/java/com/example/goppho/repositories/UserInformationRepository.java
@@ -0,0 +1,12 @@
+package com.example.goppho.repositories;
+
+
+import com.example.goppho.entities.UserEntity;
+import com.example.goppho.entities.UserInformationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserInformationRepository extends JpaRepository<UserInformationEntity, String> {
+    Optional<UserInformationEntity> findByUser(UserEntity user);
+}
