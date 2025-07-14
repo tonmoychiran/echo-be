6e64b9976329140548867f55b906c9a501b12ec5
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-14 15:55:04 +0600

    feat: username, email, name, dob added and indexed them in user entity

--- src/main/java/com/example/goppho/entities/UserEntity.java
+++ src/main/java/com/example/goppho/entities/UserEntity.java
@@ -2,11 +2,16 @@ package com.example.goppho.entities;
 
 import jakarta.persistence.*;
 
+import java.sql.Date;
 import java.time.Instant;
-import java.util.List;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users",
+indexes = {
+        @Index(name = "idx_users_user_name", columnList = "user_name"),
+        @Index(name = "idx_users_email", columnList = "email"),
+        @Index(name = "idx_users_name", columnList = "name")
+})
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -14,7 +19,16 @@ public class UserEntity {
     private String userId;
 
     @Column(length = 50, unique = true, nullable = false)
-    private String userEmail;
+    private String userName;
+
+    @Column(length = 50, unique = true, nullable = false)
+    private String email;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Date dob;
 
     @Column(nullable = false, updatable = false)
     private Long createdAt;
@@ -22,14 +36,15 @@ public class UserEntity {
     @Column(nullable = false)
     private Long lastUpdatedAt;
 
-    @OneToMany(mappedBy = "user")
-    private List<ParticipantEntity> participants;
-
-    public UserEntity(String userEmail) {
-        this.userEmail = userEmail;
+    public UserEntity(String userName, String email, String name, Date dob) {
+        this.userName = userName;
+        this.email = email;
+        this.name = name;
+        this.dob = dob;
     }
 
     public UserEntity() {
+
     }
 
     public String getUserId() {
@@ -40,23 +55,51 @@ public class UserEntity {
         this.userId = userId;
     }
 
-    public String getUserEmail() {
-        return userEmail;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
     }
 
     public Long getCreatedAt() {
         return createdAt;
     }
 
-    public long getLastUpdatedAt() {
+    public void setCreatedAt(Long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getLastUpdatedAt() {
         return lastUpdatedAt;
     }
 
-    public void setLastUpdatedAt(long lastUpdatedAt) {
+    public void setLastUpdatedAt(Long lastUpdatedAt) {
         this.lastUpdatedAt = lastUpdatedAt;
     }
 
