9b8232cff7beda58a0fab2350f1e226f3c161b76
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-14 14:25:34 +0600

    fix: user principle replaced with authentication principle

--- src/main/java/com/example/goppho/controllers/MessageController.java
+++ src/main/java/com/example/goppho/controllers/MessageController.java
@@ -3,13 +3,14 @@ package com.example.goppho.controllers;
 import com.example.goppho.entities.MessageEntity;
 import com.example.goppho.requests.MessageRequest;
 import com.example.goppho.services.MessageService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 public class MessageController {
@@ -23,17 +24,18 @@ public class MessageController {
         this.messageService = messageService;
     }
 
-    @MessageMapping("/message/{roomId}")
-    @SendTo("/topic/{roomId}")
+    @MessageMapping("/message/{conversationId}")
+    @SendTo("/topic/{conversationId}")
     public MessageEntity greeting(
-            MessageRequest messageRequest,
-            @AuthenticationPrincipal UserDetails userDetails,
-            @DestinationVariable String conversationId
+            @Valid MessageRequest messageRequest,
+            @DestinationVariable String conversationId,
+            Principal principal
     ) {
+
         return messageService.createNewMessage(
                 messageRequest,
                 conversationId,
-                userDetails.getUsername()
+                principal.getName()
         );
     }
 
