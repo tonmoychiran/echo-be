6fd866a22a8715afa0dfb1e9d08694744cf06da7
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-14 16:33:55 +0600

    feat: check email and username added

--- src/main/java/com/example/goppho/controllers/UserController.java
+++ src/main/java/com/example/goppho/controllers/UserController.java
@@ -1,14 +1,14 @@
 package com.example.goppho.controllers;
 
-import com.example.goppho.entities.UserInformationEntity;
-import com.example.goppho.requests.UserDOBUpdateRequest;
-import com.example.goppho.requests.UserNameUpdateRequest;
+import com.example.goppho.entities.UserEntity;
+import com.example.goppho.interfaces.UsernameValidator;
 import com.example.goppho.responses.GetResponse;
-import com.example.goppho.responses.Response;
 import com.example.goppho.services.UserService;
-import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.apache.coyote.BadRequestException;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,46 +21,41 @@ public class UserController {
     UserService userService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(
+            UserService userService
+    ) {
         this.userService = userService;
     }
 
-    @GetMapping("/search")
-    public ResponseEntity<UserInformationEntity> searchByName(
-            @RequestParam("name") String name
-    ) {
-        UserInformationEntity user = this.userService.getUserByName(name);
-        return ResponseEntity.ok(user);
-    }
-
     @GetMapping("/profile")
-    public ResponseEntity<GetResponse<UserInformationEntity>> getUserProfile(
+    public ResponseEntity<GetResponse<UserEntity>> getUserProfile(
             @AuthenticationPrincipal UserDetails userDetails
     ) {
-        GetResponse<UserInformationEntity> response = this.userService.getUserInformationByEmail(userDetails);
-        return new ResponseEntity<>(
-                response,
-                HttpStatus.OK
+        GetResponse<UserEntity> response = this.userService.getUserProfile(userDetails);
+        return ResponseEntity.ok(
+                response
         );
     }
 
-    @PutMapping("/name")
-    public ResponseEntity<Response> updateUserUserName(
-            @AuthenticationPrincipal UserDetails userDetails,
-            @Valid @RequestBody UserNameUpdateRequest userNameUpdateRequest
-    ) {
-        Response response = this.userService.updateUserName(userDetails, userNameUpdateRequest);
-        return ResponseEntity.ok(response);
+    @GetMapping("/check/email")
+    public ResponseEntity<Void> checkEmail(
+            @NotBlank(message = "Email is empty")
+            @Length(max = 50, message = "Email exceeds 50 characters")
+            @Email(message = "Enter a valid email")
+            @RequestParam String email
+    ) throws BadRequestException {
+        this.userService.checkEmail(email);
+        return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/dob")
-    public ResponseEntity<Response> updateUserUserDOB(
-            @AuthenticationPrincipal UserDetails userDetails,
-            @Valid @RequestBody UserDOBUpdateRequest userDOBUpdateRequest
-    ) {
-        Response response = this.userService.updateUserDOB(userDetails, userDOBUpdateRequest);
-        return ResponseEntity.ok(response);
+    @GetMapping("/check/username")
+    public ResponseEntity<Void> checkUsername(
+            @NotBlank(message = "Username is empty")
+            @Length(max = 50, message = "Username exceeds 50 characters")
+            @UsernameValidator
+            @RequestParam String username
+    ) throws BadRequestException {
+        this.userService.checkUsername(username);
+        return ResponseEntity.ok().build();
     }
-
-
 }
--- src/main/java/com/example/goppho/services/UserService.java
+++ src/main/java/com/example/goppho/services/UserService.java
@@ -2,112 +2,73 @@ package com.example.goppho.services;
 
 import com.example.goppho.entities.UserAuthOTPEntity;
 import com.example.goppho.entities.UserEntity;
-import com.example.goppho.entities.UserInformationEntity;
-import com.example.goppho.repositories.UserInformationRepository;
 import com.example.goppho.repositories.UserRepository;
-import com.example.goppho.requests.UserDOBUpdateRequest;
-import com.example.goppho.requests.UserNameUpdateRequest;
 import com.example.goppho.responses.GetResponse;
-import com.example.goppho.responses.Response;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.transaction.Transactional;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
     UserRepository userRepository;
-    UserInformationRepository userInformationRepository;
+
 
     @Autowired
     public UserService(
-            UserRepository userRepository,
-            UserInformationRepository userInformationRepository
+            UserRepository userRepository
     ) {
         this.userRepository = userRepository;
-        this.userInformationRepository = userInformationRepository;
     }
 
-    protected List<UserEntity> getUserListById(List<String> userIds) {
+    protected List<UserEntity> getUserListById(
+            List<String> userIds
+    ) {
         return this.userRepository.findAllById(userIds);
     }
 
-    protected Optional<UserEntity> getUserById(String userId) {
+    protected Optional<UserEntity> getUserById(
+            String userId
+    ) {
         return this.userRepository.findById(userId);
     }
 
-    protected Optional<UserInformationEntity> getUserInformationByUser(UserEntity user) {
-        return this.userInformationRepository.findByUser(user);
+    protected Optional<UserEntity> getUserByEmail(
+            String email
+    ){
+        return this.userRepository.findByEmail(email);
     }
 
-    protected Optional<UserInformationEntity> getUserInformationByUserId(String userId) {
-        return this.userInformationRepository.findByUserUserId(userId);
-    }
-
-    protected UserEntity getUserFromUserDetails(UserDetails userDetails) {
+    protected UserEntity getUserFromUserDetails(
+            UserDetails userDetails
+    ) {
         UserAuthOTPEntity userAuthOTPEntity = (UserAuthOTPEntity) userDetails;
         return userAuthOTPEntity.getUser();
     }
 
-    public GetResponse<UserInformationEntity> getUserInformationByEmail(UserDetails userDetails) {
-        UserAuthOTPEntity userAuthOTPEntity = (UserAuthOTPEntity) userDetails;
-        UserEntity user = userAuthOTPEntity.getUser();
-
-        Optional<UserInformationEntity> userInformation = this.getUserInformationByUser(user);
-        UserInformationEntity userInformationEntity = userInformation.orElse(null);
-
-        return new GetResponse<>("User Information", userInformationEntity);
-    }
-
-    public UserInformationEntity getUserByName(String name) {
-        Optional<UserInformationEntity> user = this.userInformationRepository.findByName(name);
-        return user.orElseThrow(() -> new EntityNotFoundException("User not found"));
-    }
-
-    public Response updateUserName(
-            UserDetails userDetails,
-            UserNameUpdateRequest userNameUpdateRequest
+    public GetResponse<UserEntity> getUserProfile(
+            UserDetails userDetails
     ) {
-        String name = userNameUpdateRequest.getName();
-
         UserEntity user = getUserFromUserDetails(userDetails);
-        Optional<UserInformationEntity> userInformation = this.getUserInformationByUser(user);
-
-        if (userInformation.isPresent()) {
-            UserInformationEntity updatableUserInformationEntity = userInformation.get();
-            updatableUserInformationEntity.setName(name);
-            this.userInformationRepository.save(updatableUserInformationEntity);
-        } else {
-            this.userInformationRepository.save(new UserInformationEntity(name, user));
-        }
-
-        return new Response("Updated");
+        return new GetResponse<>("User Information", user);
     }
 
-    public Response updateUserDOB(
-            UserDetails userDetails,
-            UserDOBUpdateRequest userDOBUpdateRequest
-    ) {
-        LocalDate localDate = userDOBUpdateRequest.getDob();
-        Date dob = Date.valueOf(localDate);
-
-        UserEntity user = getUserFromUserDetails(userDetails);
-        Optional<UserInformationEntity> userInformation = this.getUserInformationByUser(user);
-
-        if (userInformation.isEmpty()) {
-            throw new EntityNotFoundException("Update failed");
+    public void checkEmail(
+            String email
+    ) throws BadRequestException {
+        if (this.userRepository.existsByEmail(email)) {
+            throw new BadRequestException("Email exists");
         }
+    }
 
-        UserInformationEntity updatableUserInformationEntity = userInformation.get();
-        updatableUserInformationEntity.setDob(dob);
-        this.userInformationRepository.save(updatableUserInformationEntity);
-
-        return new Response("Updated");
+    public void checkUsername(
+            String username
+    ) throws BadRequestException {
+        if (this.userRepository.existsByUsername(username)) {
+            throw new BadRequestException("Username exists");
+        }
     }
 }
