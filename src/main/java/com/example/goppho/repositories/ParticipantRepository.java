14329972791c0ae42637249fae9ecdc9999ddd1a
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-14 14:28:25 +0600

    fix: used derived method instead of custom methods

--- src/main/java/com/example/goppho/repositories/ParticipantRepository.java
+++ src/main/java/com/example/goppho/repositories/ParticipantRepository.java
@@ -4,11 +4,9 @@ import com.example.goppho.entities.ConversationEntity;
 import com.example.goppho.entities.ParticipantEntity;
 import com.example.goppho.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ParticipantRepository extends JpaRepository<ParticipantEntity, String> {
-    @Query("SELECT p FROM ParticipantEntity p WHERE p.user = :user AND p.conversation = :conversationId")
     Optional<ParticipantEntity> findByUserAndConversation(UserEntity user, ConversationEntity conversation);
 }
