e77d6d9a0a9f9c398540eef19e891b31d8bbc40d
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-16 23:36:32 +0600

    refactor: changed message request shape

--- src/main/java/com/example/echo/requests/MessageRequest.java
+++ src/main/java/com/example/echo/requests/MessageRequest.java
@@ -3,10 +3,17 @@ package com.example.echo.requests;
 import jakarta.validation.constraints.NotBlank;
 
 public class MessageRequest {
+    @NotBlank(message = "Conversation Id is empty")
+    String conversationId;
+
     @NotBlank(message = "Message is empty")
     String message;
 
     public String getMessage() {
         return message;
+    }
+
+    public String getConversationId() {
+        return conversationId;
     }
 }
